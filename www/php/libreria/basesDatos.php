1bc66780fcab16b8e1af8fe47c1afdb8312b93c6
Alvaro <alvaromosquerarial@hotmail.com>
2024-01-22 19:59:00 +0100

    preparando la creación de las tablas

---
+++ www/php/libreria/basesDatos.php
@@ -0,0 +1,110 @@
+<?php
+include("php/libreria/libreria.php");
+
+
+
+
+
+function tabalaClientes()
+{
+    $conPDO = conexion();
+
+    $sql = " CREATE TABLE IF NO EXISTS  clientes (
+        id INT(6) AUTO_INCREMENT PRIMARY KEY, 
+        nombreCliente VARCHAR(30) NOT NULL,
+        seccion VARCHAR(30) NOT NULL
+        );";
+
+    $conPDO->exec($sql);
+
+    $conPDO = null;
+}
+function tabalaMaquinas()
+{
+    $conPDO = conexion();
+
+    $sql = " CREATE TABLE IF NO EXISTS  clientes (
+        id INT(6) AUTO_INCREMENT PRIMARY KEY, 
+        nombreMaquina VARCHAR(30) NOT NULL,
+        foto VARCHAR(30),
+        planos VARCHAR(30),
+        manuales VARCHAR(30),
+        despiece VARCHAR(30),
+        seccion VARCHAR(30) NOT NULL
+        );";
+
+    $conPDO->exec($sql);
+
+    $conPDO = null;
+}
+function tabalaOperarios()
+{
+    $conPDO = conexion();
+
+    $sql = " CREATE TABLE IF NO EXISTS  clientes (
+        id INT(6) AUTO_INCREMENT PRIMARY KEY, 
+        nombreOperario VARCHAR(30) NOT NULL,
+        apellido VARCHAR(30),
+        email VARCHAR(30),
+        telefono int(9),
+        seccion VARCHAR(30),
+        seccion VARCHAR(30) NOT NULL
+        );";
+
+    $conPDO->exec($sql);
+
+    $conPDO = null;
+}
+function tabalaUsuarios()
+{
+    $conPDO = conexion();
+
+    $sql = " CREATE TABLE IF NO EXISTS  clientes (
+        id INT(6) AUTO_INCREMENT PRIMARY KEY, 
+        nombreUsuario VARCHAR(30) NOT NULL,
+        rol VARCHAR(30),
+        pass VARCHAR(500) NOT NULL
+        );";
+
+    $conPDO->exec($sql);
+
+    $conPDO = null;
+}
+function tabalaMensajes()
+{
+    $conPDO = conexion();
+
+    $sql = " CREATE TABLE IF NO EXISTS  clientes (
+        id INT(6) AUTO_INCREMENT PRIMARY KEY, 
+        mensaje VARCHAR(500) NOT NULL,
+        prioridad VARCHAR(30) NOT NULL,
+        nombre VARCHAR(30),
+        zona VARCHAR(30) NOT NULL
+        );";
+
+    $conPDO->exec($sql);
+
+    $conPDO = null;
+}
+
+function tablaPartes()
+{
+    $conPDO = conexion();
+
+    $sql = " CREATE TABLE IF NO EXISTS  clientes (
+       id_ParteAveria INT PRIMARY KEY,
+       Fecha_apertura DATE,
+       Fecha_cierre DATE,
+       Zona VARCHAR(50),
+       Operario_id INT,
+       Maquina_id INT,
+       FOREIGN KEY (Operario_ID) REFERENCES Operario(id),
+       FOREIGN KEY (id) REFERENCES Maquina(id)
+        );";
+
+    $conPDO->exec($sql);
+
+    $conPDO = null;
+}
+
+//todo: revisar los nombre de los ids para poner cada uno con su tabla.
